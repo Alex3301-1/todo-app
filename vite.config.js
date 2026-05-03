1570bbace7b7608aa4484b838aac3b605e2c301e
Alex3301-1 <asus25nout@gmail.com>
2026-05-03 23:29:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/todo-app/',
-  build: {
-    rollupOptions: {
-      plugins: [{
-        writeBundle() {
-          const fs = require('fs');
-          fs.copyFileSync('dist/index.html', 'dist/404.html');
-        }
-      }]
-    }
-  }
 })
